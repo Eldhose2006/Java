55f679bc434bcc3a1d64240f0a419419d3677eab
Eldhose Abraham Emerson <eldhoseabrahamemerson2028@cs.sjcetpalai.ac.in>
2025-07-19 14:58:09 +0530

    addtwonumbers

---
+++ addtwonumbers.java
@@ -0,0 +1,7 @@
+public class addtwonumbers{
+    public static void main(String[] args){
+        int number1=5;
+        int number2=5;
+        System.out.println("sum of two numbers: " + (number1+number2));
+    }
+}
